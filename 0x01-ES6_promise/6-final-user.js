ea418c50db40343fca246a9312532d782d612e1d
Fadi Asaad <firon1222@gmail.com>
2023-11-30 03:04:00 +0200

    update: task 6: fuck my life

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => res.map(({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : reason,
+    .then((res) => res.map((o) => ({
+      status: o.status,
+      value: o.status === 'fulfilled' ? o.value : String(o.reason),
     })));
 }

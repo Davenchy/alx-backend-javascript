950283d41dcde7a9a773fc2b09fbe280f1608853
Fadi Asaad <firon1222@gmail.com>
2023-11-30 02:32:19 +0200

    update: task 6: use Promise.allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const createStateObject = (promise) => promise.then(
-  (value) => ({ status: 'fulfilled', value }),
-  (err) => ({ status: 'rejected', value: err.message }),
-);
-
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-  const stateObjects = promises.map(createStateObject);
-  const responses = Promise.all(stateObjects);
-  return responses;
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]).then((promises) => promises.map(
+    ({ status, value, reason }) => ({ status, value: value || reason.message }),
+  ));
 }

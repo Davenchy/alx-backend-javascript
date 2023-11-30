87d470373b9c0dbb2c1a962ab14fe3073faaa5b5
Fadi Asaad <firon1222@gmail.com>
2023-11-30 02:52:53 +0200

    update: task 6: try this code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(filename),
-  ]).then((promises) => promises.map(
-    ({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : reason.message,
-    }),
-  ));
+    uploadPhoto(fileName),
+  ]).then((res) =>
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    }))
+  );
 }

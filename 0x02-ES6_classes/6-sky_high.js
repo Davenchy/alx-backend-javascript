ce2a5e85bc913460be54e9ad43ae4e8113bcb823
Fadi Asaad <firon1222@gmail.com>
2023-12-01 02:39:19 +0200

    fix: task 6: evacuationWarningMessage method was defined as a getter by mistake

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -11,7 +11,7 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  get evacuationWarningMessage() {
+  evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
 }

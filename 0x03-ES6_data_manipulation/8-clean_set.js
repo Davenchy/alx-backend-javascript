02c6741f6515dbea26854335f0002981e39cc1aa
Fadi Asaad <firon1222@gmail.com>
2023-12-04 23:20:50 +0200

    add task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+  return Array.from(set)
+    .filter((item) => item.startsWith(startString))
+    .map((item) => String(item).substring(startString.length))
+    .join("-");
+}

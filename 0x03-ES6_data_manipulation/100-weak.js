b91e87ec43dae8937b931a4edad9a0f3c935e5f3
Fadi Asaad <firon1222@gmail.com>
2023-12-05 00:07:51 +0200

    fix: task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
 export const weakMap = new WeakMap();
-export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
-  const count = weakMap.get(endpoint);
+export const queryAPI = (endpoint) => {
+  let count = 1;
 
-  if (count >= 5) throw new Error('Endpoint load is high');
+  if (weakMap.has(endpoint)) {
+    count += weakMap.get(endpoint);
+    if (count > 5) throw new Error('Endpoint load is high');
+  }
 
-  weakMap.set(endpoint, count + 1);
-}
+  weakMap.set(endpoint, count);
+};

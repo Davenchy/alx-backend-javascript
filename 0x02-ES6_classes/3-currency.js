a4804037d36d5a742b3de8feaf0221694a039a94
Fadi Asaad <firon1222@gmail.com>
2024-04-11 20:26:07 +0200

    add task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new Error('Name must be a string');
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') throw new Error('Code must be a string');
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}

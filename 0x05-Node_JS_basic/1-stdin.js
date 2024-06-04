e569f98f110c6705ab93b61ff87e77a73d763bfd
Fadi Asaad <firon1222@gmail.com>
2024-06-04 16:10:10 +0300

    add: task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Holberton School, what is your name?")
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}\n`);
+  }
+});
+
+process.stdin.on('end', function() {
+  process.stdout.write('This important software is now closing\n');
+});

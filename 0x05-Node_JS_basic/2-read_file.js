deb96f389e73e9facc70d6fdba60215d10c77bcf
Fadi Asaad <firon1222@gmail.com>
2024-06-04 17:21:44 +0300

    fix(task 2): corrent the output messages

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,22 +2,25 @@ const fs = require('fs');
 
 const countStudents = (path) => {
   try {
-    const fields = fs
+    const students = fs
       .readFileSync(path, 'utf8')
       .split('\n')
       .slice(1)
       .map((s) => s.trim().split(','))
       .filter((s) => s.length === 4)
-      .map((s) => ({ name: s[0], field: s[3] }))
-      .reduce((m, s) => {
-        const fieldsMap = m;
-        if (Array.isArray(fieldsMap[s.field])) {
-          fieldsMap[s.field].push(s.name);
-        } else {
-          fieldsMap[s.field] = [s.name];
-        }
-        return m;
-      }, {});
+      .map((s) => ({ name: s[0], field: s[3] }));
+
+    const fields = students.reduce((m, s) => {
+      const fieldsMap = m;
+      if (Array.isArray(fieldsMap[s.field])) {
+        fieldsMap[s.field].push(s.name);
+      } else {
+        fieldsMap[s.field] = [s.name];
+      }
+      return m;
+    }, {});
+
+    console.log(`Number of students: ${students.length}`);
 
     for (const field of Object.keys(fields)) {
       const studentNames = fields[field];

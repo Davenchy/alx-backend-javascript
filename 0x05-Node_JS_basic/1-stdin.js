91026383cc26ee4881e2ee58fb7b87cb0aa974a7
Fadi Asaad <firon1222@gmail.com>
2024-06-04 16:15:24 +0300

    fix: linting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log("Welcome to Holberton School, what is your name?")
-process.stdin.on("readable", () => {
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}\n`);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });

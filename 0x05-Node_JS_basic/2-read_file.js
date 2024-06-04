221bdbc3744fdb2e0a3dca896ad51639ab7cac78
Fadi Asaad <firon1222@gmail.com>
2024-06-04 17:36:40 +0300

    update(task 2): suppert databases with unkown columns

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,26 +2,39 @@ const fs = require('fs');
 
 const countStudents = (path) => {
   try {
-    const students = fs
+    const lines = fs
       .readFileSync(path, 'utf8')
       .split('\n')
-      .slice(1)
-      .map((s) => s.trim().split(','))
-      .filter((s) => s.length === 4)
-      .map((s) => ({ name: s[0], field: s[3] }));
+      .map((l) => l.trim().split(','))
+      .filter((s) => s.length > 0);
 
-    const fields = students.reduce((m, s) => {
-      const fieldsMap = m;
-      if (Array.isArray(fieldsMap[s.field])) {
-        fieldsMap[s.field].push(s.name);
-      } else {
-        fieldsMap[s.field] = [s.name];
+    const [labels, ...studentsData] = lines;
+    const firstNameIndex = labels.indexOf('firstname');
+    const fieldIndex = labels.indexOf('field');
+
+    if (firstNameIndex === -1 || fieldIndex === -1) {
+      throw new Error('Invalid CSV file');
+    }
+
+    let studentCount = 0;
+    const fields = {};
+
+    for (const student of studentsData) {
+      if (student.length >= 2) {
+        studentCount += 1;
+        const field = student[fieldIndex];
+        const firstName = student[firstNameIndex];
+
+        if (Array.isArray(fields[field])) {
+          fields[field].push(firstName);
+        } else {
+          fields[field] = [firstName];
+        }
       }
-      return m;
-    }, {});
+    }
 
-    console.log(`Number of students: ${students.length}`);
-
+    console.log(`Number of students: ${studentCount}`);
+    //
     for (const field of Object.keys(fields)) {
       const studentNames = fields[field];
       console.log(`Number of students in ${field}: ${

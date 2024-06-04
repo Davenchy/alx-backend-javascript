55f1080d364cd0ad9694c8cb610edc82cfa2ca5e
Fadi Asaad <firon1222@gmail.com>
2024-06-04 16:17:41 +0300

    fix(task 1): remove newline character from the name result message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}\n`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 

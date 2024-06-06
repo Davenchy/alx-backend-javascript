558dcb39528789926c3ba12d1f7c81fae13f568d
Fadi Asaad <firon1222@gmail.com>
2024-06-07 02:38:33 +0300

    update(task 1): rewrite the functions to suite the new prototype

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,99 +4,115 @@ const assert = require('assert');
 describe('Test with type equal to SUM', function() {
   const type = 'SUM';
 
+  it('should return 6', function() {
+    assert.equal(calc(type, 1.4, 4.5), 6);
+  })
+
   it('should return 5', function() {
-    assert.equal(calc(1, 4, type), 5);
+    assert.equal(calc(type, 1, 4), 5);
+  })
+
+  it('should return 5', function() {
+    assert.equal(calc(type, 1.2, 4), 5);
   });
 
   it('should return 5', function() {
-    assert.equal(calc(1.2, 4, type), 5);
-  });
-
-  it('should return 5', function() {
-    assert.equal(calc(1.2, 4.3, type), 5);
+    assert.equal(calc(type, 1.2, 4.3), 5);
   });
 
   it('should return 6', function() {
-    assert.equal(calc(1.5, 4.3, type), 6);
+    assert.equal(calc(type, 1.5, 4.3), 6);
   });
 
   it('should return 7', function() {
-    assert.equal(calc(1.5, 4.8, type), 7);
+    assert.equal(calc(type, 1.5, 4.8), 7);
   });
 
   it('should return -2', function() {
-    assert.equal(calc(3.2, -4.8, type), -2);
+    assert.equal(calc(type, 3.2, -4.8), -2);
   });
 
   it('should return 0', function() {
-    assert.equal(calc(0.2, -0.3, type), 0);
+    assert.equal(calc(type, 0.2, -0.3), 0);
   });
 });
 
 describe('Test with type equal to SUBTRACT', function() {
   const type = 'SUBTRACT';
 
-  it('should return 3', function() {
-    assert.equal(calc(1, 4, type), 3);
+  it('should return -4', function() {
+    assert.equal(calc(type, 1.4, 4.5), -4);
+  })
+
+  it('should return -3', function() {
+    assert.equal(calc(type, 1, 4), -3);
   });
 
-  it('should return 3', function() {
-    assert.equal(calc(1.2, 4, type), 3);
+  it('should return -3', function() {
+    assert.equal(calc(type, 1.2, 4), -3);
   });
 
-  it('should return 3', function() {
-    assert.equal(calc(1.2, 4.3, type), 3);
+  it('should return -3', function() {
+    assert.equal(calc(type, 1.2, 4.3), -3);
   });
 
-  it('should return 2', function() {
-    assert.equal(calc(1.5, 4.3, type), 2);
+  it('should return -2', function() {
+    assert.equal(calc(type, 1.5, 4.3), -2);
   });
 
-  it('should return 3', function() {
-    assert.equal(calc(1.5, 4.8, type), 3);
+  it('should return -3', function() {
+    assert.equal(calc(type, 1.5, 4.8), -3);
   });
 
-  it('should return -8', function() {
-    assert.equal(calc(3.2, -4.8, type), -8);
+  it('should return 8', function() {
+    assert.equal(calc(type, 3.2, -4.8), 8);
   });
 
   it('should return 0', function() {
-    assert.equal(calc(0.2, -0.3, type), 0);
+    assert.equal(calc(type, 0.2, -0.3), 0);
   });
 });
 
 describe('Test with type equal to DIVIDE', function() {
   const type = 'DIVIDE';
 
+  it('should return 0.2', function() {
+    assert.equal(calc(type, 1.4, 4.5), 0.2);
+  })
+
   it('should return 2', function() {
-    assert.equal(calc(8, 4, type), 2);
+    assert.equal(calc(type, 8, 4), 2);
   });
 
   it('should return 2', function() {
-    assert.equal(calc(8.2, 4, type), 2);
+    assert.equal(calc(type, 8.2, 4), 2);
   });
 
   it('should return 2', function() {
-    assert.equal(calc(8.2, 4.3, type), 2);
+    assert.equal(calc(type, 8.2, 4.3), 2);
   });
 
   it('should return 1', function() {
-    assert.equal(calc(3.5, 4.3, type), 1);
+    assert.equal(calc(type, 3.5, 4.3), 1);
   });
 
   it('should return 0.4', function() {
-    assert.equal(calc(1.5, 4.8, type), 0.4);
+    assert.equal(calc(type, 1.5, 4.8), 0.4);
   });
 
   it('should return -0.6', function() {
-    assert.equal(calc(3.2, -4.8, type), -0.6);
+    assert.equal(calc(type, 3.2, -4.8), -0.6);
   });
 
   it('should return 0', function() {
-    assert.equal(calc(0.2, -1.3, type), 0);
+    assert.equal(calc(type, 0.2, -1.3), 0);
   });
 
   it('should return Error', function() {
-    assert.equal(calc(0.2, -0.3, type), 'Error');
+    assert.equal(calc(type, 0.2, -0.3), 'Error');
   });
+
+  it('should return Error', function() {
+    assert.equal(calc(type, 1.4, 0), 'Error');
+  })
 });

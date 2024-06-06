e91e8c55c2e073397c27fee16a94fc3c86e7e728
Fadi Asaad <firon1222@gmail.com>
2024-06-07 02:00:36 +0300

    update(task 0): use assert.equal

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,18 +3,19 @@ const assert = require('assert');
 
 describe('calculateNumber', function () {
   it('should return 4', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
   it('should return 5', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 
   it('should return 5', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
 
   it('should return 6', function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
+

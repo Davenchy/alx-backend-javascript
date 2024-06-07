9d7bd301f846b9485e56a086bdfd82fced953495
Fadi Asaad <firon1222@gmail.com>
2024-06-07 03:19:37 +0300

    add(task 3)

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,18 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    a = Math.round(a), b = Math.round(b);
+
+    switch (type) {
+      case 'SUM':
+        return a + b;
+      case 'SUBTRACT':
+        return a - b;
+      case 'DIVIDE':
+        return b === 0 ? 'Error' : a / b;
+      default:
+        break;
+    }
+  },
+};
+
+module.exports = Utils;

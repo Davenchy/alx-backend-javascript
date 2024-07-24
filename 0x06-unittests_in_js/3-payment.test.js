ded29beb5b62e44d3569f14a202e3a0e4ad5d13d
Fadi Asaad <firon1222@gmail.com>
2024-07-25 02:35:05 +0300

    test linting

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,14 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
-const assert = require('assert');
+const sinon = require("sinon");
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment");
+const assert = require("assert");
 
-describe('Test Utils#calculateNumber', () => {
-  it('should be called with same parameters', () => {
-    sinon.spy(Utils, 'calculateNumber');
+describe("Test Utils#calculateNumber", () => {
+	it("should be called with same parameters", () => {
+		sinon.spy(Utils, "calculateNumber");
 
-    sendPaymentRequestToApi(100, 20);
-    assert(Utils.calculateNumber.calledOnce);
-    assert(Utils.calculateNumber.calledWith('SUM', 100, 20));
-  });
+		sendPaymentRequestToApi(100, 20);
+		assert(Utils.calculateNumber.calledOnce);
+		assert(Utils.calculateNumber.calledWith("SUM", 100, 20));
+	});
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,19 +1,23 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
-const assert = require('assert');
+const sinon = require("sinon");
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment");
+const assert = require("assert");
 
-describe('Test Utils#calculateNumber', () => {
-  it('should be called with same parameters', () => {
-    const log = sinon.spy(console, 'log');
-    const calc = sinon.stub(Utils, 'calculateNumber').returns(10);
+describe("Test Utils#calculateNumber", () => {
+	it("should be called with same parameters", () => {
+		if (console.log.restore) {
+			console.log.restore();
+		}
 
-    sendPaymentRequestToApi(100, 20);
+		const log = sinon.spy(console, "log");
+		const calc = sinon.stub(Utils, "calculateNumber").returns(10);
 
-    assert(calc.calledOnce);
-    assert(calc.calledWith('SUM', 100, 20));
+		sendPaymentRequestToApi(100, 20);
 
-    assert(log.calledOnce);
-    assert(log.calledWith('The total is: 10'));
-  });
+		assert(calc.calledOnce);
+		assert(calc.calledWith("SUM", 100, 20));
+
+		assert(log.calledOnce);
+		assert(log.calledWith("The total is: 10"));
+	});
 });
